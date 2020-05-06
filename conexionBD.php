5c4225652973a2585148659924fc953c1f1329b8
Judith Rosado <jroslopz@gmail.com>
2020-05-06 13:30:02 +0200

    cambio

--- conexionBD.php
+++ conexionBD.php
@@ -1,27 +1,23 @@
 <?php
 
 class Conexion {
-   private $host;
-   private $dbname;
-   private $user;
-   private $pass;
-
-   public function __construct(){
-      $this->host = 'localhost';
-      $this->dbname = 'serieflix';
-      $this->$user = 'root';
-      $this->$pass = '2asir';
-   }
+   public $host = "localhost";
+   public $dbname = "serieflix";
+   public $user = "root";
+   public $pass = "2asir";
 
    public function Conectar() {
       try {
-         $connection = "mysql:host=" . $this->host . ";dbname=" . $this->dbname;
+         $connection = "mysql:host=$this->host;dbname=$this->dbname";
+         $user = $this->user;
+         $pass = $this->pass;
+         $options = array (
+            PDO::MYSQL_ATTR_LOCAL_INFILE => 1,
+            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
          
-         $options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                     PDO::ATTR_EMULATE_PREPARES => false];
-         
-                     $pdo = new PDO($connection, $this ->user, $this->pass, $options);
-
+         $pdo = new PDO($connection, $user, $pass, $options);
+         return $pdo;
       } catch (PDOException $e) {
          print_r("Error connection: " . $e->getMessage());
       }
