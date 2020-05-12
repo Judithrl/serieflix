b7b3cd12c7ad90111c9e890a14ef49875283acc1
Judith Rosado <jroslopz@gmail.com>
2020-05-12 11:07:48 +0200

    Lista de trabajadores eliminar o modificar

---
+++ trabajador_buscado.php
@@ -0,0 +1,41 @@
+<?php
+require_once 'ConexionBD.php';
+require_once 'Trabajador.php';
+?>
+<!DOCTYPE html>
+<html>
+    <title>SERIEFLIX</title>
+        <meta charset="UTF-8" />
+        <body>
+            <center><h1>SERIEFLIX, S.L</h1>
+            <h3>BÚSQUEDA DE TRABAJADORES</h3>
+            <br />
+            <FONT SIZE=4>
+        
+            <?php
+                $buscar = (new Trabajador())->listar(); ?>
+            <script>
+                function eliminar(COD_TRABAJADOR) {
+                    var res = window.confirm("¿Está seguro de eliminar al trabajador : "+COD_TRABAJADOR+"?");
+                    if (res) {
+                     location.href = "./controlador/generoControl.php?accion=eliminar&COD_TRABAJADOR="+COD_TRABAJADOR;;
+                    }
+                }
+            </script>
+
+            <p>¿Quiere volver a la página principal?</p>
+            <br />
+            <form action='index.php' method="post">
+                <input type="submit" name="menu" value="Volver al menú" />
+            
+            <?php
+              if (isset($_POST['menu'])) {
+                header('Location:./index.php');
+              }  
+            ?>
+            </form>
+            </FONT>
+            </center>
+        </body>
+</html>
+
