334ca4ddf6dfbca50ca0bcc5b602dd4a20c3ff85
Judith Rosado <jroslopz@gmail.com>
2020-05-12 11:08:43 +0200

    Trabajador modificado

---
+++ trabajador_modificado.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <title>SERIEFLIX</title>
+    <meta charset="UTF-8" />
+    <body>
+        <center><h1>SERIEFLIX, S.L</h1>
+        <h3>¡EMPIECE A CREAR SU EMPRESA!</h3>
+        <br />
+        <form action='' method="post">
+            <FONT SIZE=4>
+            <p>El nuevo trabajador se ha modificado correctamente.<br />
+            ¿Quiere volver a la página principal?</p>
+            <br />
+            <input type="submit" name="menu" value="Volver al menú" />
+            </FONT>
+            <?php
+              if (isset($_POST['menu'])) {
+                header('Location:./index.php');
+              }  
+            ?>
+        </form>
+    </center>
+    </body>
+</html>
+</html>
